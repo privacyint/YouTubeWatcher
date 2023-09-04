a0a638da06f37b1792aa8a082f27952e3ba8bf10
Ed Geraghty <edg@privacyinternational.org>
2023-09-04 09:54:33 +0100

    Ditch unused imports

--- src/docker_firefox.py
+++ src/docker_firefox.py
@@ -1,8 +1,3 @@
-import logging
-import time
-
-import docker
-from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.remote.webdriver import WebDriver
 
