b2eb0f2f068916550e94fc41f31dc76b7c5757e5
Ed Geraghty <edg@privacyinternational.org>
2023-09-22 10:50:13 +0100

    Pull `video_chooser` out of its lambda

--- src/watch_strategy.py
+++ src/watch_strategy.py
@@ -14,17 +14,20 @@ def watch_strategy(driver: WebDriver, search_terms: list, channel_url: str, dura
     start_time = datetime.now()
     # Watch for the duration
     while datetime.now() < (start_time + timedelta(minutes=duration)):
-        video_chooser = lambda: random.choice(
-            [
-                # Pick a random video from the channel
-                random.choice(get_channel_videos(driver, channel_url)),
-                # Pick a random video from a random search term
-                random.choice(do_search(driver, random.choice(search_terms))),
-            ]
-        )
-
-        video = video_chooser()
+        video = video_chooser(driver, search_terms, channel_url)
         logging.info(f"Watching {video.title}")
         # Watch the video
         driver.get(video.url)
         watch_current_video(driver)
+
+
+def video_chooser(driver: WebDriver, search_terms: list, channel_url: str):
+    """Return a random video from either the passed channel OR from a randomly chosen search term"""
+    return random.choice(
+        [
+            # Pick a random video from the channel
+            random.choice(get_channel_videos(driver, channel_url)),
+            # Pick a random video from a random search term
+            #random.choice(do_search(driver, random.choice(search_terms))),
+        ]
+    )
