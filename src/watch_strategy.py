0bb0480b57c0212d738265ab4786ac1729c77326
Ed Geraghty <edg@privacyinternational.org>
2023-11-14 14:28:37 +0000

    Complete rewrite of our watching strategy

--- src/watch_strategy.py
+++ src/watch_strategy.py
@@ -1,8 +1,12 @@
 import logging
 import random
+import time
 from datetime import datetime, timedelta
 
+from selenium.webdriver import ActionChains
+from selenium.webdriver.common import keys
 from selenium.webdriver.remote.webdriver import WebDriver
+from selenium.webdriver.support.wait import WebDriverWait
 
 from src.youtube_shorts import do_search, get_channel_videos, watch_current_video
 
@@ -14,13 +18,26 @@ def watch_strategy(driver: WebDriver, search_terms: list, channel_url: str, dura
     start_time = datetime.now()
     logging.info(f"Starting @ {start_time}")
 
-    # Watch for the duration
-    while datetime.now() < (start_time + timedelta(minutes=duration)):
-        video = video_chooser(driver, search_terms, channel_url)
-        logging.info(f"Watching {video.title}")
-        # Watch the video
-        driver.get(video.url)
-        watch_current_video(driver)
+    #Find our starting point, watch the video
+    video = video_chooser(driver, search_terms, channel_url)
+    driver.get(video.url)
+
+    # Not particularly DRY but we can revisit
+    if duration == 0:
+        logging.info(f"Watching until the heat death of the universe")
+
+        while True:
+            logging.info(f"Watching {video.title}")
+            time.sleep(30)
+            nextVideo = ActionChains(driver).send_keys('n')
+            nextVideo.perform()
+    else:
+        # Watch for the duration
+        while datetime.now() < (start_time + timedelta(minutes=duration)):
+            logging.info(f"Watching {video.title}")
+            # Watch the video
+            driver.get(video.url)
+            watch_current_video(driver)
 
 
 def video_chooser(driver: WebDriver, search_terms: list, channel_url: str):
