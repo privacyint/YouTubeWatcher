9b43c1ec699ba2f4629c8b0869a2bdb7b2e78912
Ed Geraghty <edg@privacyinternational.org>
2023-11-27 15:21:09 +0000

    Log out to a file

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ from src.youtube_shorts import close_cookie_popup
 
 def main():
     logging.getLogger().setLevel(logging.INFO)
+    logging.basicConfig(filename='yt-shorts.log', encoding='utf-8', level=logging.DEBUG)  # Defaults to append
 
     # Setup Selenium web driver
     parser = get_arg_parser()
